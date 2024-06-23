5b3d3d02db7ab98b93cbce3ef61f7e7fe8e7af36
manish-kumar-2002 <manishkumar8923245598@gmail.com>
2024-06-23 13:47:23 +0530

    add slider in banner

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,110 @@
 $(document).ready(function(){
-    $('.banner-slider').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
-            dots:true
-        });
+    // $('.banner-slider').slick({
+    //         slidesToShow: 1,
+    //         slidesToScroll: 1,
+    //         arrows: false,
+    //         dots:true
+    //     });
+    const swiper = new Swiper('.swiper', {
+      // Optional parameters
+      // direction: 'vertical',
+      effect: "fade",
+      loop: true,
+      // autoplay: {
+      //   delay: 2500,
+      //   disableOnInteraction: false,
+      // },
+    
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    
+      // Navigation arrows
+      // navigation: {
+      //   nextEl: '.swiper-button-next',
+      //   prevEl: '.swiper-button-prev',
+      // },
+    
+      // And if we need scrollbar
+      // scrollbar: {
+      //   el: '.swiper-scrollbar',
+      // },
+    });
+    const swiperAnother = new Swiper('.swiper-another', {
+      fadeEffect: { crossFade: true },
+      effect: "fade",
+
+      loop: true,
+     
+    });
+
+
+    swiper.controller.control = swiperAnother;
+    swiperAnother.controller.control = swiper;
 });
+
+// $(document).ready(function () {
+//   $(window).scroll(function () {
+//       var scrollTop = $(window).scrollTop();
+//       var windowHeight = $(window).height();
+//       var bannerHeight = $('.banner').height();
+
+//       if (scrollTop < bannerHeight) {
+//           var opacity = scrollTop / bannerHeight;
+
+//           $('.banner').css({
+//               'background': `linear-gradient(to bottom, rgba(0, 0, 0, ${opacity}), rgba(0, 0, 0, 1))`
+//           });
+//       } else {
+//           $('.banner').css({
+//               'background': 'black'
+//           });
+//       }
+
+//       if (scrollTop <= 0) {
+//           $('.banner').css({
+//               'background': 'linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1))'
+//           });
+//       }
+//   });
+// });
+
+
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+      var scrollTop = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      var bannerHeight = $('.banner').height();
+
+      if (scrollTop < bannerHeight) {
+          var opacity = scrollTop / bannerHeight;
+
+          $('.banner::after').css({
+            'content':`""`,
+              'background': `linear-gradient(to bottom, rgba(0, 0, 0, ${opacity}), rgba(0, 0, 0, 1))`
+          });
+      } else {
+          $('.banner::after').css({
+            'content':`""`,
+              'background': 'black'
+          });
+      }
+
+      if (scrollTop <= 0) {
+          $('.banner::after').css({
+              'background': 'linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1))'
+          });
+      }
+  });
+});
+
+
+
+
+
 const body = document.querySelector('body'),
       sidebar = body.querySelector('nav'),
       toggle = body.querySelector(".toggle"),
